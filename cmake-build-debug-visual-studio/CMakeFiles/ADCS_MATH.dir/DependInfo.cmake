23074e0e6be919246e5302ac09e5bdb130cbc0bf
Ruben <ruben.e.sanchez.a@gmail.com>
2021-10-29 21:53:46 -0400

    CMAKE updated

---
+++ cmake-build-debug-visual-studio/CMakeFiles/ADCS_MATH.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Ruben/OneDrive/Desktop/space/adcs-math/src/main.cpp" "C:/Users/Ruben/OneDrive/Desktop/space/adcs-math/cmake-build-debug-visual-studio/CMakeFiles/ADCS_MATH.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../extern/eigen"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-visual-studio/CMakeFiles/ADCS_MATH.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ADCS_MATH.exe"
+  "ADCS_MATH.exe.manifest"
+  "ADCS_MATH.lib"
+  "ADCS_MATH.pdb"
+  "CMakeFiles/ADCS_MATH.dir/src/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ADCS_MATH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
